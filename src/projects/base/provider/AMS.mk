905a8d04303cf6395531531af881c6bf7b1fb96f
letsdoitnow <ahlininv@gmail.com>
2021-02-12 01:52:41 +0300

    cmake for ovsocket ovlibrary provider

--- src/projects/base/provider/AMS.mk
+++ src/projects/base/provider/AMS.mk
@@ -1,16 +1,4 @@
 LOCAL_PATH := $(call get_local_path)
-include $(DEFAULT_VARIABLES)
-
-LOCAL_TARGET := provider
-
-LOCAL_SOURCE_FILES := $(LOCAL_SOURCE_FILES) \
-    $(call get_sub_source_list,pull_provider) \
-    $(call get_sub_source_list,push_provider)
-
-LOCAL_HEADER_FILES := $(LOCAL_HEADER_FILES) \
-    $(call get_sub_header_list,pull_provider) \
-    $(call get_sub_header_list,push_provider)
-
-$(call add_pkg_config,srt)
-
-include $(BUILD_STATIC_LIBRARY)
+$( eval mkdir -p $(LOCAL_PATH) )
+$( eval cmake -H $(LOCAL_PATH) -B $(LOCAL_PATH)/build )
+$( eval make -f $(LOCAL_PATH)/build )
