956ae374b0da41c17810f01f8d9d4239f466f148
letsdoitnow <ahlininv@gmail.com>
2021-02-14 16:29:36 +0300

    cmake works

--- src/projects/utils.cmake
+++ src/projects/utils.cmake
@@ -1,5 +1,5 @@
 
-function(add_external_library LIBNAME)
+function(add_external_library TGT LIBNAME)
     find_package(PkgConfig REQUIRED)
     message(STATUS PkgConfig_FOUND: ${PkgConfig_FOUND})
 
@@ -21,7 +21,22 @@ function(add_external_library LIBNAME)
     message( STATUS "${NAME_INCLUDE_DIRS}: "       ${${NAME_INCLUDE_DIRS}})
     message( STATUS "${NAME_LDFLAGS_HERE}: "       ${${NAME_LDFLAGS_HERE}})
 
-    target_link_libraries(${TARGET}      PUBLIC ${${NAME_LIBS}})
-    target_link_directories(${TARGET}    PUBLIC  ${${NAME_INCLUDE_DIRS}} )
-    target_include_directories(${TARGET} PUBLIC  ${${NAME_INCLUDE_DIRS}} )
+    target_link_libraries(${TGT}      PUBLIC ${${NAME_LIBS}})
+    target_link_directories(${TGT}    PUBLIC  ${${NAME_INCLUDE_DIRS}} )
+    target_include_directories(${TGT} PUBLIC  ${${NAME_INCLUDE_DIRS}} )
+endfunction()
+
+function (add_common_compile_options TGT)
+    target_compile_options(${TGT}
+            PUBLIC -std=c++17
+            PRIVATE -Wfatal-errors -Wno-inconsistent-missing-override -Wno-macro-redefined
+            )
+endfunction()
+
+function (include_common_dirs TGT PATH_UP)
+    target_include_directories(${TGT}
+            PRIVATE
+            ${PATH_UP}projects
+            ${PATH_UP}projects/third_party
+            )
 endfunction()
